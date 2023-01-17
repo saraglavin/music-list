8801cddb383be5a4153bcba4261d6525b3002347
Sara Glavin <hej@saraglavin.se>
2023-01-17 11:00:36 +0100

    Added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC6zGBbYmrs2GEZPLxQC4mFEa5JYnRtn5g",
+    authDomain: "music-list-c9ee0.firebaseapp.com",
+    projectId: "music-list-c9ee0",
+    storageBucket: "music-list-c9ee0.appspot.com",
+    messagingSenderId: "216860189755",
+    appId: "1:216860189755:web:800fc832c508d95dd6e81e"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+//init services
+
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+const timeStamp = firebase.firestore.FieldValue.serverTimestamp
+
+
+export { projectFirestore, projectAuth, timeStamp }
